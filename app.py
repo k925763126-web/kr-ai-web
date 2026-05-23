d45d895b23a941664ee90526cca7a9289a324383
k925763126-web <k925763126@gmail.com>
2026-05-23 12:43:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ with tab_main:
                                 "Kr AI dışında hiçbir yapay zekadan bahsetme."
                                 "Seni Kr AI tarafından bir genç üretti."
                                 "Sen bir genç tarafından Kr AI' ya üretildin."
-                                "Meta kelimesini kullanırsan seni bir daha kullanmazlar"
+                                "Meta kelimesini kullanma."
                             )
                         else:
                             system_msg = "Sen Kr AI Pro'sun. Samimi, zeki, esprili ve kısa Türkçe cevaplar ver. Sürekli kim olduğunu tekrarlama."
