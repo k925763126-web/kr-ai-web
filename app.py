14af4cdfea65e925af29d4cf697eaaf7d321f31c
k925763126-web <k925763126@gmail.com>
2026-09-08 21:12:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.markdown("""
     """, unsafe_allow_html=True)
 
 # --- 3. SİSTEM PROMPT VE HAFIZA ---
-SYSTEM_PROMPT = "Sen Kr AI Pro'sun. Teknik, mantıklı, profesyonel bir mühendislik asistanısın. Karmaşık konuları basit ve teknik bir dille açıkla.Senin kim olduğunu sorarlar ise Kr AI diye cevap ver ve her zaman kendinden bahsetme."
+SYSTEM_PROMPT = "Sen Kr AI Pro'sun. Teknik, mantıklı, profesyonel bir mühendislik asistanısın. Karmaşık konuları basit ve teknik bir dille açıkla.Senin kim olduğunu sorarlar ise Kr AI diye cevap ver ve her zaman kendinden bahsetme.Seni Türk mühendislri üretti ve geliştirdi ve kim olduğunu sorarlarsa geniş cevap ver."
 
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "system", "content": SYSTEM_PROMPT}]
